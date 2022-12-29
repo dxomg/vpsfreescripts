bae347043070b7f11c151923b09ef43f76957354
dxomg <101947415+dxomg@users.noreply.github.com>
2022-12-29 15:47:31 -0600

    Update install.sh

--- vpsfreescripts/minecraftscript/install.sh
+++ vpsfreescripts/minecraftscript/install.sh
@@ -16,74 +16,79 @@ echo "4 -> 1.8.8"
 echo ""
 read -p "Enter minecraft version: " minecraftv
 
-if [ "$minecraftv" = 1 ]  ; then
-    echo "Selected: ${minecraftv} > 1.18.2"
-    mkdir mcserver
-    cd mcserver
-    wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/388/downloads/paper-1.18.2-388.jar
-    echo "eula=true" > eula.txt
-    apt install openjdk-17-jre-headless -y
-    pm2 start "java -jar server.jar"
-    echo "Server is up and running!"
-    echo "to check the logs do:"
-    echo "pm2 logs 1"
-    echo ""
-    echo "to attach to the console do:"
-    echo "pm2 attach 1"
-    echo ""
-    echo "For more commands please check the help for pm2"
+case $minecraftv in
+    1)
+        echo "Selected: ${minecraftv} > 1.18.2"
+        mkdir mcserver
+        cd mcserver
+        wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.18.2/builds/388/downloads/paper-1.18.2-388.jar
+        echo "eula=true" > eula.txt
+        apt install openjdk-17-jre-headless -y
+        pm2 start "java -jar server.jar"
+        echo "Server is up and running!"
+        echo "to check the logs do:"
+        echo "pm2 logs 1"
+        echo ""
+        echo "to attach to the console do:"
+        echo "pm2 attach 1"
+        echo ""
+        echo "For more commands please check the help for pm2"
+        ;;
 
-elif [ "$minecraftv" = 2 ]  ; then
-    echo "Selected: ${minecraftv} > 1.16.5"
-    mkdir mcserver
-    cd mcserver
-    wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar
-    echo "eula=true" > eula.txt
-    apt install openjdk-17-jre-headless -y
-    pm2 start "java -jar server.jar"
-    echo "Server is up and running!"
-    echo "to check the logs do:"
-    echo "pm2 logs 1"
-    echo ""
-    echo "to attach to the console do:"
-    echo "pm2 attach 1"
-    echo ""
-    echo "For more commands please check the help for pm2"
+    2)
+        echo "Selected: ${minecraftv} > 1.16.5"
+        mkdir mcserver
+        cd mcserver
+        wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar
+        echo "eula=true" > eula.txt
+        apt install openjdk-17-jre-headless -y
+        pm2 start "java -jar server.jar"
+        echo "Server is up and running!"
+        echo "to check the logs do:"
+        echo "pm2 logs 1"
+        echo ""
+        echo "to attach to the console do:"
+        echo "pm2 attach 1"
+        echo ""
+        echo "For more commands please check the help for pm2"
+        ;;
 
-elif [ "$minecraftv" = 3 ]  ; then
-    echo "Selected: ${minecraftv} > 1.12.2"
-    mkdir mcserver
-    cd mcserver
-    wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar
-    echo "eula=true" > eula.txt
-    apt install openjdk-8-jdk-headless -y
-    pm2 start "java -jar server.jar"
-    echo "Server is up and running!"
-    echo "to check the logs do:"
-    echo "pm2 logs 1"
-    echo ""
-    echo "to attach to the console do:"
-    echo "pm2 attach 1"
-    echo ""
-    echo "For more commands please check the help for pm2"
+    3)
+        echo "Selected: ${minecraftv} > 1.12.2"
+        mkdir mcserver
+        cd mcserver
+        wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.12.2/builds/1620/downloads/paper-1.12.2-1620.jar
+        echo "eula=true" > eula.txt
+        apt install openjdk-8-jdk-headless -y
+        pm2 start "java -jar server.jar"
+        echo "Server is up and running!"
+        echo "to check the logs do:"
+        echo "pm2 logs 1"
+        echo ""
+        echo "to attach to the console do:"
+        echo "pm2 attach 1"
+        echo ""
+        echo "For more commands please check the help for pm2"
+        ;;
 
-elif [ "$minecraftv" = 4 ]  ; then
-    echo "Selected: ${minecraftv} > 1.8.8"
-    mkdir mcserver
-    cd mcserver
-    wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar
-    echo "eula=true" > eula.txt
-    apt install openjdk-8-jdk-headless -y
-    pm2 start "java -jar server.jar"
-    echo "Server is up and running!"
-    echo "to check the logs do:"
-    echo "pm2 logs 1"
-    echo ""
-    echo "to attach to the console do:"
-    echo "pm2 attach 1"
-    echo ""
-    echo "For more commands please check the help for pm2"
-fi
+    4)
+        echo "Selected: ${minecraftv} > 1.8.8"
+        mkdir mcserver
+        cd mcserver
+        wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar
+        echo "eula=true" > eula.txt
+        apt install openjdk-8-jdk-headless -y
+        pm2 start "java -jar server.jar"
+        echo "Server is up and running!"
+        echo "to check the logs do:"
+        echo "pm2 logs 1"
+        echo ""
+        echo "to attach to the console do:"
+        echo "pm2 attach 1"
+        echo ""
+        echo "For more commands please check the help for pm2"
+        ;;
+esac
 
 echo "__________________________________________________________"
 echo ""
