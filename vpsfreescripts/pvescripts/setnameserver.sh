e225d0981c4518ef8c1cb25c3128d776eff4ade1
dxomg <101947415+dxomg@users.noreply.github.com>
2023-01-06 04:16:40 -0600

    Update to working dns64

--- vpsfreescripts/pvescripts/setnameserver.sh
+++ vpsfreescripts/pvescripts/setnameserver.sh
@@ -5,6 +5,6 @@ containers=$(pct list | grep -Eo "^[0-9]+")
 
 # Set the name server for each container
 for container in $containers; do
-  pct set $container --nameserver "2001:67c:2b0::4 2001:67c:2b0::6"
+  pct set $container --nameserver "2a00:1098:2c::1 2a00:1098:2b::1 2a01:4f8:c2c:123f::1"
 done
 done
