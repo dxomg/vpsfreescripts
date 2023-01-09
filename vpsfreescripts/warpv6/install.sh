a13e8fa1f59c724d9aa37a42408337d58f94a711
dxomg <101947415+dxomg@users.noreply.github.com>
2023-01-09 14:58:40 -0600

    Update install.sh

--- vpsfreescripts/warpv6/install.sh
+++ vpsfreescripts/warpv6/install.sh
@@ -1,10 +1,13 @@
-echo "======================================================"
-echo ""
-echo "When script starts connecting to warp please do Ctrl+C"
-echo "and then connect again"
-echo ""
-echo "======================================================"
-
-wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
-
-echo "Endpoint = [2001:67c:2b0:db32:0:1:a29f:c001]:2408" > /etc/wireguard/wgcf.conf
+if warp -v foo >/dev/null 2>&1; then
+    echo "Endpoint = [2001:67c:2b0:db32:0:1:a29f:c001]:2408" > /etc/wireguard/wgcf.conf
+    echo "Patch was successfully applied"
+else
+    echo "======================================================"
+    echo ""
+    echo "When script starts connecting to warp please do Ctrl+C"
+    echo "and then run this script again"
+    echo ""
+    echo "======================================================"
+    sleep 5
+    wget -N https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh && bash menu.sh
+fi
