887146fbc58a2ff55ff36d8ae33862fccc65d830
dxomg <101947415+dxomg@users.noreply.github.com>
2023-01-09 14:31:47 -0600

    Update add warp and fix sum stuff

--- main.sh
+++ main.sh
@@ -4,36 +4,39 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo "#######################################################################################"
-echo ""
-echo "* VPSFREE.ES SCRIPTS"
-echo "*"
-echo "* Copyright (C) 2022 - 2023, VPSFREE.ES"
-echo "*"
-echo "*"
+echo "#"
+echo "#                                  VPSFREE.ES SCRIPTS"
+echo "#"
+echo "#                           Copyright (C) 2022 - 2023, VPSFREE.ES"
+echo "#"
+echo "#"
 echo "#######################################################################################"
-echo "*"
+echo ""
 echo "* [0] Install Minecraft Server Files"
-echo "* [1] Install PufferPanel"
-echo "* [2] Install WebPage"
-echo "* [3] Install Basic Packages"
-echo "* [4] Exit"
+echo "* [1] Install Warp to enable Ipv4 on Ipv6 only"
+echo "* [2] Install PufferPanel"
+echo "* [3] Install WebPage"
+echo "* [4] Install Basic Packages"
+echo "* [5] Exit"
 
-read -p "Enter Input (1-3): " input
+read -p "Enter Input (0-5): " input
 
 case $input in
 
     0)
     apt update && wget https://github.com/dxomg/vpsfreescripts/raw/main/vpsfreescripts/minecraftscript/install.sh && bash install.sh;;
     1)
+    curl -s https://github.com/dxomg/vpsfreescripts/raw/main/vpsfreescripts/warpv6/install.sh | bash;;
+    2)
     apt update && apt install curl -y
     curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
     sudo apt-get install pufferpanel
     sudo systemctl enable pufferpanel
     sudo pufferpanel user add
     sudo systemctl enable --now pufferpanel;;
-    2)
-    bash <(curl -s https://raw.githubusercontent.com/dxomg/vpsfreescripts/main/vpsfreescripts/websitescript/install.sh);;
     3)
+    bash <(curl -s https://raw.githubusercontent.com/dxomg/vpsfreescripts/main/vpsfreescripts/websitescript/install.sh);;
+    4)
     apt install git
     apt install neofetch
     echo "##############################"
@@ -41,5 +44,5 @@ case $input in
     echo "Installed git and neofetch"
     echo ""
     echo "##############################";;
-    4) echo "Exit" && exit;;
+    5) echo "Exit" && exit;;
 esac
