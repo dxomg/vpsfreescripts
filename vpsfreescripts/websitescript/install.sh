216d0a80db64ad7621de70870b854297c03caa64
dxomg <101947415+dxomg@users.noreply.github.com>
2023-01-11 18:32:47 -0600

    Fix stuff

--- vpsfreescripts/websitescript/install.sh
+++ vpsfreescripts/websitescript/install.sh
@@ -3,18 +3,50 @@ apt install python3-pip
 pip install flask
 mkdir website && cd website
 
-echo "from flask import Flask, render_template
-app = Flask(__name__)
+echo "########################################"
+echo ""
+echo "If you are using vpsfree's free service"
+echo "select 1 else select 0"
+echo ""
+echo "########################################"
+echo ""
+echo "* 0) Ipv4 (0.0.0.0)"
+echo "* 1) Ipv6 (::)"
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+read -p "> " ip
 
-if __name__ == '__main__':
-    app.run('0.0.0.0',80) # <----- ip & port" > main.py
 
-mkdir templates/
-touch templates/index.html
-echo "Hello World!  -  it seems to be woking!!!" > templates/index.html
+case $ip in
 
-echo "Now just run 'python3 website/main.py'"
+    0)
+    echo "from flask import Flask, render_template
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return render_template('index.html')
+
+    if __name__ == '__main__':
+        app.run('0.0.0.0',80) # <----- ip & port" > main.py
+
+    mkdir templates/
+    touch templates/index.html
+    echo "Hello World!  -  it seems to be woking!!!" > templates/index.html
+
+    echo "Now just run 'python3 website/main.py'";;
+    1)
+    echo "from flask import Flask, render_template
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_world():
+        return render_template('index.html')
+
+    if __name__ == '__main__':
+        app.run('::',80) # <----- ip & port" > main.py
+
+    mkdir templates/
+    touch templates/index.html
+    echo "Hello World!  -  it seems to be woking!!!" > templates/index.html
+
+    echo "Now just run 'python3 website/main.py'"
