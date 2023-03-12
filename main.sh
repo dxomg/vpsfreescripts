0a6dd897bf3bcad1f1488d052f99b242b0a67578
BunnyNotFoundXD <123856811+BunnyNotFoundXD@users.noreply.github.com>
2023-03-12 16:41:08 +0530

    Added color codes

--- main.sh
+++ main.sh
@@ -5,29 +5,45 @@ fi
 
 apt install sudo -y
 # color codes
+RESTORE='\033[0m'
 
+RED='\033[00;31m'
+GREEN='\033[00;32m'
+YELLOW='\033[00;33m'
+BLUE='\033[00;34m'
+PURPLE='\033[00;35m'
+CYAN='\033[00;36m'
+LIGHTGRAY='\033[00;37m'
+
+LRED='\033[01;31m'
+LGREEN='\033[01;32m'
+LYELLOW='\033[01;33m'
+LBLUE='\033[01;34m'
+LPURPLE='\033[01;35m'
+LCYAN='\033[01;36m'
+WHITE='\033[01;37m'
 
 echo "#######################################################################################"
 echo "#"
-echo "#                                  VPSFREE.ES SCRIPTS"
+echo "#                             $RED     VPSFREE.ES SCRIPTS" 
 echo "#"
-echo "#                           Copyright (C) 2022 - 2023, VPSFREE.ES"
+echo "#                       $BLUE    Copyright (C) 2022 - 2023, VPSFREE.ES"
 echo "#"
 echo "#"
 echo "#######################################################################################"
 echo ""
-echo "* [0] Install Minecraft Server Files"
-echo "* [1] Install Warp to enable Ipv4 on Ipv6 only"
-echo "* [2] Download Playit files to make tunnels to V4 & V6"
-echo "* [3] Install PufferPanel"
-echo "* [4] Launch Website"
-echo "* [5] Subdomain Scanner"
-echo "* [6] Install CRDP/XRDP"
-echo "* [7] Install Basic Packages"
-echo "* [8] Install Pterodactyl Panel"
-echo "* [9] Exit"
+echo "* $GREEN [0] Install Minecraft Server Files"
+echo "* $GREEN [1] Install Warp to enable Ipv4 on Ipv6 only"
+echo "* $GREEN [2] Download Playit files to make tunnels to V4 & V6"
+echo "* $GREEN [3] Install PufferPanel"
+echo "* $GREEN [4] Launch Website"
+echo "* $GREEN [5] Subdomain Scanner"
+echo "* $GREEN [6] Install CRDP/XRDP"
+echo "* $GREEN [7] Install Basic Packages"
+echo "* $GREEN [8] Install Pterodactyl Panel"
+echo "* $RED [9] Exit"
 
-read -p "Enter Input (0-7): " input
+read -p "$LBLUE Enter Input (0-7): " input
 
 case $input in
 
